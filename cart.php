20c2c9948c86b101df7311f36775946a9750be54
thesmallwan123 <12321.post@gmail.com>
2020-11-12 11:01:00 +0100

    bug fixed als cart leef is

--- cart.php
+++ cart.php
@@ -3,7 +3,10 @@
 
 // Get cookies of items in cart 
 // setcookie("carItem[$ID of item], $amount);
-$cartItems = $_COOKIE["cartItem"];
+$cartItems=NULL;
+if(isset($_COOKIE["cartItem"])){
+    $cartItems = $_COOKIE["cartItem"];
+}
 
 function controllItem($artikelID)
 {
@@ -30,34 +33,36 @@ include __DIR__ . "/header.php";
 ?>
 <div id="Wrap">
     <?php
-    foreach ($cartItems as $artikelID => $amount) {
-        $item = controllItem($artikelID);
-    ?>
-        <div class="cartRow">
-            <div class="rowLeft">
-                <!-- ID and Image -->
-                <img class="productImage" src="Public/StockItemIMG/<?php echo $item[0]['ImagePath']; ?>">
-                <div class="productID">ID: <?php echo $item[0]["StockItemID"]; ?></div>
+    if(isset($cartItems)){
+        foreach ($cartItems as $artikelID => $amount) {
+            $item = controllItem($artikelID);
+        ?>
+            <div class="cartRow">
+                <div class="rowLeft">
+                    <!-- ID and Image -->
+                    <img class="productImage" src="Public/StockItemIMG/<?php echo $item[0]['ImagePath']; ?>">
+                    <div class="productID">ID: <?php echo $item[0]["StockItemID"]; ?></div>
+                </div>
+                <div class="rowMiddle">
+                    <!-- Name, Description and Supply -->
+                    <div class="productName">Name: <?php echo $item[0]["StockItemName"] ?></div>
+                    <div class="productSearchDetails">Details: <?php echo $item[0]["SearchDetails"] ?></div>
+                    <div class="productQuantity">Quantity: <?php echo $item[0]["QuantityOnHand"] ?></div>
+                </div>
+                <div class="rowRight">
+                    <!-- Price(incl BTW), Amount, Remove and add button -->
+                    <div class="productPrice">Price: <?php echo $item[0]["RecommendedRetailPrice"] ?> (including BTW)</div>
+
+
+
+                    <!-- vanaf hier Jeremy -->
+
+
+
+                </div>
             </div>
-            <div class="rowMiddle">
-                <!-- Name, Description and Supply -->
-                <div class="productName">Name: <?php echo $item[0]["StockItemName"] ?></div>
-                <div class="productSearchDetails">Details: <?php echo $item[0]["SearchDetails"] ?></div>
-                <div class="productQuantity">Quantity: <?php echo $item[0]["QuantityOnHand"] ?></div>
-            </div>
-            <div class="rowRight">
-                <!-- Price(incl BTW), Amount, Remove and add button -->
-                <div class="productPrice">Price: <?php echo $item[0]["RecommendedRetailPrice"] ?> (including BTW)</div>
-
-
-
-                <!-- vanaf hier Jeremy -->
-
-
-
-            </div>
-        </div>
-    <?php
+        <?php
+        }
     }
     ?>
 </div>
