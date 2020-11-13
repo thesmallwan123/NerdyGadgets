f1d9d7d2babef202ae3774dc6c6cbec5fa656eb3
JWGrimme <janwillemgrimme@hotmail.com>
2020-11-13 16:39:10 +0100

    order.php is volledig afgemaakt, pay.php is zo goed als af.

--- pay.php
+++ pay.php
@@ -1,10 +1,24 @@
 <?php
 include __DIR__ . "/header.php";
-?>
-<!DOCTYPE html>
-<head>
-    <link rel="stylesheet" href="Public/CSS/Style.css" type="text/css">
-</head>
-<body>
 
-</body>
+$deliveryDate = date("d/m/Y", time() + 86400);
+$totaalprijs = $_SESSION["totaalPrijs"];
+$totaalprijs = 1;
+?>
+
+<div id="Wrap">
+    <div class="confirmationTextP">
+
+        <p>U moet <?php print($totaalprijs); ?> betalen.</p>
+    <p>Dit bedrag is inclusief BTW</p>
+    <p>Uw bestelling wordt op <?php print($deliveryDate); ?> geleverd. </p>
+    <p>Op uw ingevoerde adres. (adres gegevens)</p>
+        <?php
+        print($_POST["straat"]. " ");
+        print($_POST["huisnummer"]. " ");
+        print($_POST["postcode"]. " ");
+        print($_POST["woonplaats"]. " ");
+        ?>
+
+    </div>
+</div>
