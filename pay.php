65dce1d75323b4d1a14536b67199723b37e851c4
JWGrimme <janwillemgrimme@hotmail.com>
2020-11-13 19:40:53 +0100

    tijschriftformulier en pay.php afgemaakt

--- pay.php
+++ pay.php
@@ -1,7 +1,10 @@
 <?php
 include __DIR__ . "/header.php";
 
+/*calculating delivery time*/
 $deliveryDate = date("d/m/Y", time() + 86400);
+
+/*calculating total price*/
 $totaalprijs = $_SESSION["totaalPrijs"];
 $totaalprijs = 1;
 ?>
@@ -12,13 +15,21 @@ $totaalprijs = 1;
         <p>U moet <?php print($totaalprijs); ?> betalen.</p>
     <p>Dit bedrag is inclusief BTW</p>
     <p>Uw bestelling wordt op <?php print($deliveryDate); ?> geleverd. </p>
-    <p>Op uw ingevoerde adres. (adres gegevens)</p>
+    <p>Op uw ingevoerde adres.</p>
         <?php
         print($_POST["straat"]. " ");
         print($_POST["huisnummer"]. " ");
         print($_POST["postcode"]. " ");
         print($_POST["woonplaats"]. " ");
         ?>
+        <br>
+        <br>
+        <div class="toConfirmation">
+            <form action="confirmation.php">
+                <input type="submit" name="submit" value="Betalen" class="toConfirmationButton">
+            </form>
+        </div>
 
     </div>
 </div>
+
