035adf6801eeb8cc7c71f1c4336392c5e792a732
thesmallwan123 <12321.post@gmail.com>
2020-11-16 12:29:53 +0100

    lege winkelmand nettere notatie

--- cart.php
+++ cart.php
@@ -13,120 +13,131 @@ $taxTotaal = 0;
 // Haal sessie op
 if (isset($_SESSION["cart"]) && !empty($_SESSION["cart"])) {
     $winkelwagenartikelen = $_SESSION["cart"];
-}
-else{
+} else {
     $winkelwagenartikelen = "";
 }
 
 // Wijzigen winkelmand
-if(isset($_GET["id"])){
+if (isset($_GET["id"])) {
     $ID = $_GET["id"];
     $cartItems = $_SESSION['cart'];
-        if(isset($_GET["function"])){
-            $function = $_GET["function"];
-            if($function == "deleteItem"){
-                deleteItem($ID, $cartItems);
-            }
-            elseif($function == "decreaseItem"){
-                decreaseItem($ID, $cartItems);
-            }
-            elseif($function == "increaseItem"){
-                increaseItem($ID, $cartItems);
-            }
+    if (isset($_GET["function"])) {
+        $function = $_GET["function"];
+        if ($function == "deleteItem") {
+            deleteItem($ID, $cartItems);
+        } elseif ($function == "decreaseItem") {
+            decreaseItem($ID, $cartItems);
+        } elseif ($function == "increaseItem") {
+            increaseItem($ID, $cartItems);
         }
+    }
 }
 
 // Verwijderen artikel
-function deleteItem($ID, $cartItems){
-    if(array_key_exists($ID, $cartItems)) {
+function deleteItem($ID, $cartItems)
+{
+    if (array_key_exists($ID, $cartItems)) {
         unset($cartItems[$ID]);
         $_SESSION['cart'] = $cartItems;
-        ?> <script>window.location.replace('./cart.php')</script> <?php
-    }
-}
+?> <script>
+            window.location.replace('./cart.php')
+        </script> <?php
+                }
+            }
 
-// Verminderen artikel
-function decreaseItem($ID, $cartItems){
-    if(array_key_exists($ID, $cartItems)) {
-        $cartItems[$ID] -= 1;
-        $_SESSION['cart'] = $cartItems;
-        ?> <script>window.location.replace('./cart.php')</script> <?php
-    }
-}
+            // Verminderen artikel
+            function decreaseItem($ID, $cartItems)
+            {
+                if (array_key_exists($ID, $cartItems)) {
+                    $cartItems[$ID] -= 1;
+                    $_SESSION['cart'] = $cartItems;
+                    ?> <script>
+            window.location.replace('./cart.php')
+        </script> <?php
+                }
+            }
 
-// Vermeerderen artikel
-function increaseItem($ID, $cartItems){
-    if(array_key_exists($ID, $cartItems)) {
-        $cartItems[$ID] += 1;
-        $_SESSION['cart'] = $cartItems;
-        ?> <script>window.location.replace('./cart.php')</script> <?php
-    }
-}
+            // Vermeerderen artikel
+            function increaseItem($ID, $cartItems)
+            {
+                if (array_key_exists($ID, $cartItems)) {
+                    $cartItems[$ID] += 1;
+                    $_SESSION['cart'] = $cartItems;
+                    ?> <script>
+            window.location.replace('./cart.php')
+        </script> <?php
+                }
+            }
 
 
-// Haal items op uit DB
-function controllItem($artikelID){
-    include("connect.php");
-    $Query = "
+            // Haal items op uit DB
+            function controllItem($artikelID)
+            {
+                include("connect.php");
+                $Query = "
             SELECT si.StockItemID, StockItemName, QuantityOnHand, SearchDetails, colorID, TaxRate AS taxRate, ROUND((RecommendedRetailPrice*(1+(TaxRate/100))), 2) AS RecommendedRetailPrice, ImagePath
             FROM stockitems si
             LEFT JOIN stockitemimages sii ON si.StockItemID = sii.StockItemID
             INNER JOIN stockitemholdings sih ON si.StockItemID = sih.StockItemID
             WHERE si.StockItemID = ?";
 
-    $Statement = mysqli_prepare($Connection, $Query);
-    mysqli_stmt_bind_param($Statement, 'i', $artikelID); // i = integer; s = string;
-    mysqli_stmt_execute($Statement);
+                $Statement = mysqli_prepare($Connection, $Query);
+                mysqli_stmt_bind_param($Statement, 'i', $artikelID); // i = integer; s = string;
+                mysqli_stmt_execute($Statement);
 
-    $result = mysqli_stmt_get_result($Statement);
-    $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                $result = mysqli_stmt_get_result($Statement);
+                $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    return $result;
-}
+                return $result;
+            }
 
-// Bereken prijs per artikel
-function calcPriceRow($totaalPrijsRow, $prijs, $aantal){
-    $totaalPrijsRow = $prijs * $aantal;  
-    return $totaalPrijsRow;
-}
+            // Bereken prijs per artikel
+            function calcPriceRow($totaalPrijsRow, $prijs, $aantal)
+            {
+                $totaalPrijsRow = $prijs * $aantal;
+                return $totaalPrijsRow;
+            }
 
-// Bereken BTW per artikel 
-function calcTaxRow($taxArtikel, $totaalPrijsRow){
-    $taxArtikel = $taxArtikel / 100;
-    $taxRow = $totaalPrijsRow * $taxArtikel;
-    return $taxRow;
-}
+            // Bereken BTW per artikel 
+            function calcTaxRow($taxArtikel, $totaalPrijsRow)
+            {
+                $taxArtikel = $taxArtikel / 100;
+                $taxRow = $totaalPrijsRow * $taxArtikel;
+                return $taxRow;
+            }
 
-// Bereken totaalprijs
-function calcPriceTotal($prijsRegel, $totaalPrijsExVerz){
-    foreach ($prijsRegel as $id => $prijs) {
-        $totaalPrijsExVerz += $prijs;
-    }
-    return $totaalPrijsExVerz;
-}
+            // Bereken totaalprijs
+            function calcPriceTotal($prijsRegel, $totaalPrijsExVerz)
+            {
+                foreach ($prijsRegel as $id => $prijs) {
+                    $totaalPrijsExVerz += $prijs;
+                }
+                return $totaalPrijsExVerz;
+            }
 
-// Bereken prijs inclusief verzending
-function calcIncVerz($totaalPrijsExVerz){
-    if($totaalPrijsExVerz < 30){
-        $totaalPrijsIncVerz = $totaalPrijsExVerz + 4.95;
-        $_SESSION["totaalPrijs"] = $totaalPrijsIncVerz;
-        return $totaalPrijsIncVerz;
-    }
-    else {
-        $_SESSION["totaalPrijs"] = $totaalPrijsExVerz;
-        return $totaalPrijsExVerz;
-    }
-}
+            // Bereken prijs inclusief verzending
+            function calcIncVerz($totaalPrijsExVerz)
+            {
+                if ($totaalPrijsExVerz < 30) {
+                    $totaalPrijsIncVerz = $totaalPrijsExVerz + 4.95;
+                    $_SESSION["totaalPrijs"] = $totaalPrijsIncVerz;
+                    return $totaalPrijsIncVerz;
+                } else {
+                    $_SESSION["totaalPrijs"] = $totaalPrijsExVerz;
+                    return $totaalPrijsExVerz;
+                }
+            }
 
-// Bereken Eindbedrag BTW
-function calcTax($taxArr, $taxTotaal){
-    foreach($taxArr as $id => $taxRow){
-        $taxTotaal = $taxTotaal + $taxRow;
-    }
-    return ROUND($taxTotaal,2);
-}
+            // Bereken Eindbedrag BTW
+            function calcTax($taxArr, $taxTotaal)
+            {
+                foreach ($taxArr as $id => $taxRow) {
+                    $taxTotaal = $taxTotaal + $taxRow;
+                }
+                return ROUND($taxTotaal, 2);
+            }
 
-?>
+                    ?>
 <div id="Wrap">
     <div class="row returnRow">
         <div class="col-2">
@@ -144,7 +155,7 @@ function calcTax($taxArr, $taxTotaal){
             array_push($prijsRegel, $totaalPrijsRow);
             $taxRow = calcTaxRow($artikel[0]["taxRate"], $totaalPrijsRow);
             array_push($taxArr, $taxRow);
-            ?>
+    ?>
             <div class="cartRow">
                 <div class="rowLeft">
                     <!-- ID and Image -->
@@ -169,12 +180,12 @@ function calcTax($taxArr, $taxTotaal){
                     <div class="row knoppenRow">
                         <!-- Delete item -->
                         <div class="col-3">
-                                <a href="cart.php?id=<?php echo $artikelID ?>&function=deleteItem">
-                                    <i class="far fa-trash-alt"></i></a>
-                            </div>
+                            <a href="cart.php?id=<?php echo $artikelID ?>&function=deleteItem">
+                                <i class="far fa-trash-alt"></i></a>
+                        </div>
 
                         <!-- Disable minus button when amount <= 1 -->
-                        <?php if($amount <= 1){?>
+                        <?php if ($amount <= 1) { ?>
                             <div class="col-3">
 
                             </div>
@@ -201,23 +212,22 @@ function calcTax($taxArr, $taxTotaal){
         $totaalPrijsTax = calcTax($taxArr, $taxTotaal);
         ?>
         <div class="verzendKosten">
-            Verzenkosten: 
-                <?php $totaalPrijsIncVerz = calcIncVerz($totaalPrijsExVerz); 
-                if($totaalPrijsIncVerz > $totaalPrijsExVerz){
-                    echo "4.95";
-                    }
-                else{
-                 echo "0.00";   
-                }
-                ?>
+            Verzenkosten:
+            <?php $totaalPrijsIncVerz = calcIncVerz($totaalPrijsExVerz);
+            if ($totaalPrijsIncVerz > $totaalPrijsExVerz) {
+                echo "4.95";
+            } else {
+                echo "0.00";
+            }
+            ?>
             <br>
         </div>
         <div class="BTW">
-            BTW (Al bij prijs inbegrepen): 
-                <?php  
-                    echo $totaalPrijsTax;
-                ?>
-                <br>
+            BTW (Al bij prijs inbegrepen):
+            <?php
+            echo $totaalPrijsTax;
+            ?>
+            <br>
         </div>
         <br>
         <div class="totalPrice">
@@ -238,15 +248,16 @@ function calcTax($taxArr, $taxTotaal){
                 <input type="submit" class="col-1 bestelButton" name="bestel" value="Bestel">
             </div>
         </form>
-        <?php
-    } 
-    else {
-        ?>
-            <div class="row">
-                <h1 class="winkelmandLeeg">De winkelwagen is leeg</h1>
-            </div>
-            <div class="row terugText">Ga terug naar de vorige pagina</div>
-        <?php
+    <?php
+    } else {
+    ?>
+        <div class="row">
+            <h1 class="winkelmandLeeg">De winkelwagen is leeg</h1>
+        </div>
+        <div class="row" style="width: 100%;">
+                <div class="terugText" style="width: 100%; text-align: center;">Ga terug naar de vorige pagina</div>
+        </div>
+<?php
     } ?>
 
 </div>
