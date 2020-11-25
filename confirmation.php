697aaed74db73d4931031d49abb0a1ed1e2a5813
JWGRimme <janwillemgrimme@hotmail.com>
2020-11-25 21:03:58 +0100

    Bezig gegaan met bedrag invoeren op pay.php en de bevestiging daarvan

--- confirmation.php
+++ confirmation.php
@@ -2,9 +2,17 @@
 /* calculating delivery date with the date of today + 1 day*/
 $deliveryDate = date("d/m/Y", time() + 86400);
 
+/*calculating total price*/
+$totaalprijs = $_SESSION["totaalPrijs"];
+// $totaalprijs = 1;
+
 if (isset($_POST["submit"])) {
     session_destroy();
     header("Location: ./index.php");
+}
+
+if (isset($_POST["terug"])){
+    header("Location: ./order.php");
 }
 
 
@@ -19,21 +27,42 @@ if (isset($_POST["submit"])) {
 <body>
 <div class="confirmationTextHeader">
 
+    <?php
+    if (isset($_POST["bevestiging"]) == $totaalprijs) {
+?>
+
     <h1>Succes!</h1>
 
 </div>
 
 <div class="confirmationTextP">
-
+<p> De bedragen komen overeen met elkaar.</p>
 <p> Uw bestelling is succesvol afgerond.</p>
 <p> Op <?php print($deliveryDate); ?> wordt uw bestelling bezorgd.</p>
 <!--<p> Er wordt een factuur naar uw e-mailadres gestuurd.</p>-->
 </div>
 
 <div class="backToShop">
-    <form method="POST">
+    <form action="index.php">
         <input type="submit" name="submit" value="Ga terug naar de website" class="backToShopButton">
     </form>
 </div>
+<?php
+}
+else{
+?>
+<div class="confirmationTextP"></div>
+<p> de bedragen komen niet overeen ga terug naar de gegevenspagina.</p>
+<div class="backToShop">
+    <form action="order.php">
+        <input type="submit" name="terug" value="Ga terug naar de gegevenspagina" class="backToShopButton">
+    </form>
+</div>
+</div>
+<?php
+}
+?>
+
 
 </body>
+
--- pay.php
+++ pay.php
@@ -12,7 +12,7 @@ $totaalprijs = $_SESSION["totaalPrijs"];
 <div id="Wrap">
     <div class="confirmationTextP">
 
-        <p>U moet <?php print($totaalprijs); ?> betalen.</p>
+        <p>U moet €<?php print($totaalprijs); ?>,- betalen.</p>
     <p>Dit bedrag is inclusief BTW</p>
     <p>Uw bestelling wordt op <?php print($deliveryDate); ?> geleverd. </p>
     <p>Op uw ingevoerde adres:</p>
@@ -23,12 +23,22 @@ $totaalprijs = $_SESSION["totaalPrijs"];
         print($_POST["woonplaats"]. " ");
         ?>
         <br>
-        <br>
-        <div class="toConfirmation">
-            <form action="confirmation.php">
-                <input type="submit" name="submit" value="Betalen" class="toConfirmationButton">
-            </form>
-        </div>
+    <form method="post">
+        <div class="bestelRow">
+            <div class="col-12"
+                <label for="bevestiging"> Bevestig de totale prijs om te betalen</label><br>
+                <input class="opmaakPayForm" type="text" id="bevestiging" name="bevestiging" required>
+             </div>
+         </div>
+    </form>
+    <p class="boldText">LET OP! de bedragen moeten overeen komen!</p>
+    <br>
+    <br>
+    <div class="toConfirmation">
+        <form action="confirmation.php">
+            <input type="submit" name="submit" value="Betalen" class="toConfirmationButton">
+        </form>
+    </div>
 
     </div>
 </div>
