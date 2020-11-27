efee8275bb8bde4d4ab6f74ef480aa7e3b1be0d1
GewoonJasper <jasperintveld@hotmail.nl>
2020-11-27 11:29:26 +0100

    database geupdate

--- Database/nerdygadgetstest.sql
+++ Database/nerdygadgetstest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 22 nov 2020 om 16:12
+-- Gegenereerd op: 27 nov 2020 om 11:28
 -- Serverversie: 10.4.14-MariaDB
 -- PHP-versie: 7.4.9
 
@@ -42,6 +42,38 @@ CREATE TABLE `account` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Gegevens worden geëxporteerd voor tabel `account`
+--
+
+INSERT INTO `account` (`accountnr`, `email`, `firstname`, `infix`, `surname`, `gender`, `street`, `streetnr`, `postalcode`, `city`, `password`) VALUES
+(2, 'jasperintvel@hotmail.nl', 'Jasper', 'in \'t', 'Veld', 'M', 'Populierenplein', '11', '8102JK', 'Raalte', 'test123'),
+(3, 'jasperintvelddd@hotmail.nl', 'Jasper', 'in \'t', 'Veld', 'V', 'Populierenplein', '11', '8102JK', 'Raalte', 'test123'),
+(4, 'jasperitveld@hotmail.nl', 'Jasper', 'in \'t', 'Veld', 'M', 'Populierenplein', '11', '8102JK', 'Raalte', 'ffff'),
+(5, 'jasperintveld@hotma.nl', '1', '1', '1', 'M', '1', '1', '8102JK', '1', '1'),
+(6, '', '', '', '', '', '', '', '', '', ''),
+(7, '', '', '', '', '', '', '', '', '', ''),
+(8, '', '', '', '', '', '', '', '', '', ''),
+(9, '', '', '', '', '', '', '', '', '', ''),
+(10, '', '', '', '', '', '', '', '', '', ''),
+(11, '', '', '', '', '', '', '', '', '', ''),
+(12, '', '', '', '', '', '', '', '', '', ''),
+(13, '', '', '', '', '', '', '', '', '', ''),
+(14, '', '', '', '', '', '', '', '', '', ''),
+(15, '', '', '', '', '', '', '', '', '', ''),
+(16, '', '', '', '', '', '', '', '', '', ''),
+(17, '', '', '', '', '', '', '', '', '', ''),
+(18, 'dd@ss.dd', '1', '1', '1', 'man', 'dd', '11', 'dd', 'dd', '1'),
+(19, 'dd@ss.ddd', '1', '1', '1', 'man', 'dd', '11', 'dd', 'dd', '1'),
+(20, 'jasperi@hotmail.nl', 'Jasper', 'in \'t', 'Veld', 'man', 'Populierenplein', '11', '8102JK', 'Raalte', 'd'),
+(21, 'jaspeld@hotmail.nl', 'Jasper', 'dd', 'Veld', 'man', 'Populierenplein', '11', '8102JK', 'Raalte', 'dd'),
+(22, 'jasperineldd@honl.nl', 'Jasper', 'in \'t', 'Veld', 'man', 'Populierenplein', '11', '8102JK', 'Raalte', 'dd'),
+(23, 'dd@dd.d', 'Jasper', 'in \'t', 'Veld', 'man', 'dd', '11', 'dd', 'dd', 'ff'),
+(24, 'd@ss.ddd', 'Jasper', 'in \'t', 'Veld', 'man', 'dd', '11', 'dd', 'dd', 'd'),
+(25, 'jastveld@hotmail.nl', 'Jasper', 'in \'t', 'Veld', 'man', 'Populierenplein', '11', '8102JK', 'Raalte', 'dd'),
+(26, 'jasperintveld@hotma.k', 'Jasper', 'in \'t', 'Veld', 'man', 'Populierenplein', '11', '8102JK', 'Raalte', 'd'),
+(27, 'jasperintveld@hotmail.nl', 'Jasper', 'in \'t', 'Veld', 'man', 'Populierenplein', '11', '8102JK', 'Raalte', 'test');
+
+--
 -- Indexen voor geëxporteerde tabellen
 --
 
@@ -59,7 +91,7 @@ ALTER TABLE `account`
 -- AUTO_INCREMENT voor een tabel `account`
 --
 ALTER TABLE `account`
-  MODIFY `accountnr` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `accountnr` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
