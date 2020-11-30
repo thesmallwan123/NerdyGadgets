9d9e0cba329be3199fedc0b68942c48ff07ac10a
JWGRimme <janwillemgrimme@hotmail.com>
2020-11-30 20:51:17 +0100

    footer gemaakt en begin gemaakt klantenservice pagina

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+
+<div class="footer">
+    <div class="toCustomerService">
+        <form method="post" action="customerService.php">
+            <input type="submit" name="toCustomerService" value="Klantenservice" class="toCustomerServiceButton">
+        </form>
+    </div>
+</div>
