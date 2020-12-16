d95cee72c8780862ff221f361bc98eeeb3c5efe8
GewoonJasper <jasperintveld@hotmail.nl>
2020-12-16 10:31:01 +0100

    connection2 weg

--- connect.php
+++ connect.php
@@ -1,25 +1,13 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // Set MySQLi to throw exceptions
 try {
-    $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets");
+    $Connection = mysqli_connect("localhost", "klant", "klantgebruiker", "nerdygadgets");
     mysqli_set_charset($Connection, 'latin1');
     $DatabaseAvailable = true;
 } catch (mysqli_sql_exception $e) {
     $DatabaseAvailable = false;
 }
 if (!$DatabaseAvailable) {
-    ?><h2>Website wordt op dit moment onderhouden.</h2><?php
-    die();
-}
-
-try {
-    $Connection2 = mysqli_connect("localhost", "root", "", "nerdygadgetstest");
-    mysqli_set_charset($Connection2, 'latin1');
-    $Database2Available = true;
-} catch (mysqli_sql_exception $e) {
-    $Database2Available = false;
-}
-if (!$Database2Available) {
     ?><h2>Website wordt op dit moment onderhouden.</h2><?php
     die();
 }
