f03e34f98116ce53b910649ef2f9896f32bc01d3
GewoonJasper <jasperintveld@hotmail.nl>
2020-12-16 11:22:32 +0100

    CS

--- customerService.php
+++ customerService.php
@@ -33,28 +33,30 @@ if (isset($_POST["sendMailCustomerService"])) {
 }
 
 ?>
-<div class="container orderPageContainer">
-    <h3>Neem contact met ons op door het volgende formulier in te vullen.</h3>
+<div class="tekstBovenCS">
+    <h1>Neem contact met ons op door het volgende formulier in te vullen</h1>
+</div>
+<div class="container servicePageContainer">
     <!-- <form method="post" action="pay.php"> -->
     <form method="post">
         <div class="row orderRow">
             <div class="col-5">
                 <label for="voornaam"> Voornaam</label><br>
-                <input class="opmaakOrder" type="text" id="voornaam" name="voornaam" value="<?php print($voornaam); ?>" required>
+                <input class="opmaakOrder" type="text" id="voornaam" name="voornaam" value="<?php print($voornaam); ?>" placeholder="Voornaam" required>
             </div>
             <div class="col-2">
                 <label for="tussenvoegsel"> Tussenvoegsel</label>
-                <input class="opmaakOrder" type="text" id="tussenvoegsel" name="tussenvoegsel" value="<?php print($tussenvoegsel); ?>">
+                <input class="opmaakOrder" type="text" id="tussenvoegsel" name="tussenvoegsel" value="<?php print($tussenvoegsel); ?>" placeholder="Tussenvoegsel">
             </div>
             <div class="col-5">
                 <label for="achternaam"> Achternaam </label>
-                <input class="opmaakOrder" type="text" id="achternaam" name="achternaam" value="<?php print($achternaam); ?>" required>
+                <input class="opmaakOrder" type="text" id="achternaam" name="achternaam" value="<?php print($achternaam); ?>" placeholder="Achternaam" required>
             </div>
         </div>
         <div class="row orderRow">
             <div class="col-12">
                 <label for="email">Email</label>
-                <input class="opmaakOrder" type="email" id="email" name="email" value="<?php print($email); ?>" required>
+                <input class="opmaakOrder" type="email" id="email" name="email" value="<?php print($email); ?>" placeholder="Email" required>
             </div>
         </div>
         <div class="row orderRow">
