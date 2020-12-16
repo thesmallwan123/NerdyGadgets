2e700569d31cfed98e942f9e11ef452592a310ef
thesmallwan123 <12321.post@gmail.com>
2020-12-16 11:04:26 +0100

    footer class button update

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <div class="footer">
     <div class="toCustomerService">
         <form method="post" action="customerService.php">
-            <input type="submit" name="toCustomerService" value="Klantenservice button" class="toCustomerServiceButton">
+            <input type="submit" name="toCustomerService" value="Klantenservice button" class="toCustomerService Button">
         </form>
     </div>
 </div>
